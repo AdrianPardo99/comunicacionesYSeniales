1100b9f69a8ac91ff814c73e5c32523f5edc0958
Adrian González Pardo <gozapaadr@gmail.com>
2020-02-26 07:12:39 -0600

    Descripcion de la practica y primer commit remoto

---
+++ practica1Audio/source.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* La practica consta de abrir un archivo wav y a traves de este programa reducir el volumen de la senial de entrada a un 50% del valor que tiene en el archivo y guardar el archivo con otro nombre, ambos archivos son manejados como argumentos... */
